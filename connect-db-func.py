60773dafbd9ec1596491f526e7b84a33151aedfe
xanuzik <kizulash@gmail.com>
2022-07-26 18:42:40 +0900

    Update connect-db-func.py

--- connect-db-func.py
+++ connect-db-func.py
@@ -23,51 +23,56 @@ def db_search_func(connection, query):
     return result
     connection.close()
 
-ip_tobe_added = "10.3.0.10"
-uuid_tobe_added = "1111-2dasd-234easdf"
+ip_tobe_added = "1.2.3.4"
+uuid_tobe_added = "1234544"
 
-insert_ip_only = "INSERT INTO NODES.nodeinfo (ip) VALUES (%s)"
-insert_ip_uuid = "INSERT INTO NODES.nodeinfo (ip,uuid) VALUES (%s,%s)"
-insert_ip_bin =  "INSERT into NODES.nodeinfo (ip,IPv4BIN) VALUES (%s,INET6_ATON(%s))" #last two arguments are the same, using the IP twice
-update_uuid = """UPDATE nodeinfo SET uuid = %s WHERE ip = %s"""
-update_ip = """UPDATE nodeinfo SET ip = %s WHERE uuid = %s"""
-list_ip_uuid = "SELECT ip, uuid FROM nodeinfo"
+query_insert_ip_only = "INSERT INTO NODES.nodeinfo (ip) VALUES (%s)"
+query_insert_ip_uuid = "INSERT INTO NODES.nodeinfo (ip,uuid) VALUES (%s,%s)"
+query_insert_ip_bin =  "INSERT into NODES.nodeinfo (ip,IPv4BIN) VALUES (%s,INET6_ATON(%s))" #last two arguments are the same, using the IP twice
+query_update_uuid = """UPDATE nodeinfo SET uuid = %s WHERE ip = %s"""
+query_update_ip = """UPDATE nodeinfo SET ip = %s WHERE uuid = %s"""
+query_list_ip_uuid = "SELECT ip, uuid FROM nodeinfo"
 
 db_connection = conndbmod.connecting_to_db()
 
 #db_crud_func(db_connection, insert_ip_bin, ip_tobe_added, ip_tobe_added)
 
 list_ip_column = "SELECT ip FROM nodeinfo"
-ip_list = (db_search_func(db_connection,list_ip_uuid))
+ip_list = (db_search_func(db_connection,query_list_ip_uuid)) #get a list of ip and uuid
 
-print(ip_list)
+print(f"ip list is {ip_list}")
 print(len(ip_list))
 
 
 # To judge if the new IP/UUID already exits in the DB
-# def check_existence(sql_ip_uuid_list, )
 
-i = 0
-while i < len(ip_list):
-    if ip_tobe_added == ip_list[i][0]: #update uuid according to ip address, [i][0] is ip address
-        if uuid_tobe_added == ip_list[i][1]: #[i][1] is uuid
-            continue
+def db_pre_deduplicate(connection, para_query_update_uuid, para_query_update_ip, db_uuid_ip_list, new_ip, new_uuid): #1 the list of uuid and ip(may add timestamp later?), 2and3 are the info of the new node
+    
+    i = 0
+    
+    while i < len(db_uuid_ip_list):
+        
+        if new_ip == db_uuid_ip_list[i][0]: #update uuid according to ip address, [i][0] is ip address
+            if new_uuid == db_uuid_ip_list[i][1]: #[i][1] is uuid
+                print("Same host in DB!")
+                break
+            else:
+                print("Same ip in DB, updating UUID!")
+                db_crud_func(connection, para_query_update_uuid, new_uuid, new_ip)
+                i=i+1
+            
         else:
-            db_crud_func(db_connection, update_uuid, ip_list[i][1], ip_list[i][0])
-    else:
-        if uuid_tobe_added != ip_list[i][1]:
-            continue
-        else:
-            db_crud_func(db_connection, update_ip, ip_list[i][0], ip_list[i][1])
-        print(ip_list[i][0])
-        print(i)
-        i = i+1
+            if new_uuid != db_uuid_ip_list[i][1]:
+                print("This is a whole new host! Will be added to the DB")
+                i=i+1
+            else:
+                print("Same UUID in DB, updating Host!")
+                db_crud_func(connection, para_query_update_ip, new_ip, new_uuid)
+                i = i+1
+            # print(db_uuid_ip_list[i][0])
+            # print(i)
+            # i = i+1
 
-#def
+db_pre_deduplicate(db_connection, query_update_uuid, query_update_ip, ip_list, ip_tobe_added, uuid_tobe_added)
 
-
-
-#print(results)
-#print(type(results))
-#test_function(conn, "SELECT * FROM nodeinfo")
 db_connection.close()
